441c8cd85e806ccea21840c156b564dfb312739d
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-13 11:12:17 +0530

    Create spammer.sh

---
+++ spammer.sh
@@ -0,0 +1,77 @@
+pkg install figlet
+pkg install lolcat
+clear
+figlet -f slant "EvilSpammer"|lolcat
+sleep 2
+clear                                                                                                                                                                                                               
+echo -e "\e[1;33m      ####### #     # ### #         #####  ######     #    #     # #     # ####### ######  "  
+echo -e "\e[1;33m      #       #     #  #  #        #     # #     #   # #   ##   ## ##   ## #       #     # "
+echo -e "\e[1;31m      #       #     #  #  #        #       #     #  #   #  # # # # # # # # #       #     # "
+echo -e "\e[1;31m      #####   #     #  #  #         #####  ######  #     # #  #  # #  #  # #####   ######  "
+echo -e "\e[1;33m      #        #   #   #  #              # #       ####### #     # #     # #       #   #   "
+echo -e "\e[1;33m      #         # #    #  #        #     # #       #     # #     # #     # #       #    #  "
+echo -e "\e[1;32m      #######    #    ### #######   #####  #       #     # #     # #     # ####### #     # "
+echo -e "\e[1;32    
+echo -e "\e[1;32m      Created By JOHN KENER "
+
+
+echo -e "\e[1;31m________________________________________________________________________"                                                                                                                          
+echo
+echo -e "\e[99m                   [1]                                          |\e[0m";                                                                                                                                                             
+echo -e "\e[98m                   [2]  Spam Item 02               |\e[0m";
+echo -e "\e[97m                   [3]  Spam Item 03               |\e[0m";                                                                                                                                                             
+echo -e "\e[95m                   [4]  Spam Item 04               |\e[0m";
+echo -e "\e[96m                   [5]  Exit           |\e[0m";
+echo
+function req(){
+    cd ..
+    cd cws
+    echo -e "\e[1;32m         දැන් ,මේක සම්පූර්ණයෙන්ම COPY  කරගන්න....!!!     \e[0m";
+    sleep 3
+}
+function copy(){
+    
+
+    
+    
+    rm -rf cws
+    mkdir cws
+    cd spm
+
+
+}
+read -p' SPAM වර්ගය තෝරන්න :-' userinput1;
+if [ "${userinput1:-}" = "1" ]
+then
+    copy
+    cp spam1 /data/data/com.termux/files/home/Evil-spammer/cws
+    req
+    cat spm1
+fi
+if [ "${userinput1:-}" = "2" ]
+then
+    copy
+    cp spam2 /data/data/com.termux/files/home/Evil-spammer/cws
+    req
+    cat spm2
+fi
+if [ "${userinput1:-}" = "3" ]
+then
+    copy
+    cp spam3 /data/data/com.termux/files/home/Evil-spammer/cws
+    req
+    cat spm3
+fi
+if [ "${userinput1:-}" = "4" ]
+then
+    copy
+    cp spam4 /data/data/com.termux/files/home/Evil-spammer/cws
+    req
+    cat spm4
+fi
+if [ "${userinput1:-}" = "5" ]
+then
+echo THANK YOU  ...! |lolcat
+    cd
+fi
+
