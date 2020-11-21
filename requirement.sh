792e9fa58d8846aae9b86c7fe56fd6258cbd5fee
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-21 12:26:12 +0530

    Create requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic
+figlet Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m 😈 DESTROY YOUR ENIMY,,DON'T BE KIND 💀..!!!"
+echo
+echo -e "\e[1m\e[32m Created by :\e[33m JOHN KENER (OWNER OF CYBER WARRIORS COMMUNITY) "
