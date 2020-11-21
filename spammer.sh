adb18c0ef68a68f5efa9239a162e95d40fe05f74
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-21 12:27:05 +0530

    Create spammer.sh

---
+++ spammer.sh
@@ -0,0 +1,202 @@
+clear
+echo
+figlet CYBER | lolcat -a -d 5
+figlet WARRIORS | lolcat -a -d 5
+sleep 1
+clear    
+echo -e "\e[1;31m ________________________________________________________________________"
+echo -e "\e[1;31m ________________________________________________________________________"
+echo
+echo
+echo -e "\e[1;34m ####### #     # ### #       ";
+echo -e "\e[1;34m #       #     #  #  #       ";
+echo -e "\e[1;34m #       #     #  #  #       ";
+echo -e "\e[1;34m #####   #     #  #  #       ";
+echo -e "\e[1;34m #        #   #   #  #       ";
+echo -e "\e[1;34m #         # #    #  #       ";
+echo -e "\e[1;34m #######    #    ### ####### ";
+echo 
+echo -e "\e[1;34m  #####  ######     #    #     # #     # ####### ######     ";
+echo -e "\e[1;34m #     # #     #   # #   ##   ## ##   ## #       #     #   ";
+echo -e "\e[1;34m #       #     #  #   #  # # # # # # # # #       #     #   ";
+echo -e "\e[1;34m  #####  ######  #     # #  #  # #  #  # #####   ######    ";
+echo -e "\e[1;34m       # #       ####### #     # #     # #       #   #     ";
+echo -e "\e[1;34m #     # #       #     # #     # #     # #       #    #    ";
+echo -e "\e[1;34m  #####  #       #     # #     # #     # ####### #     #   ";
+echo
+echo -e "              Created By JOHN KENER "
+echo -e "              @ CYBER WARRIORS "
+echo -e "\e[1;31m ________________________________________________________________________"
+echo -e "\e[1;31m _______________________________________________________________________"                                                                                                                          
+echo
+echo -e "\e[1;33m                 [1]  Normal spam                      ";                                                                                                                                                             
+echo -e "\e[1;33m                 [2]  Powerful spam                    ";
+echo -e "\e[1;33m                 [3]  Read more spam                   ";                                                                                                                                                             
+echo -e "\e[1;33m                 [4]  Mobile number spam               ";
+echo -e "\e[1;33m                 [5]  ABOUT US                         ";
+echo -e "\e[1;33m                 [6]  EXIT                             ";  
+echo
+function req(){
+    cd ..
+    cd cws
+    echo -e "  දැන් ෴ මේක සම්පූර්ණයෙන්ම COPY  කරගන්න....!!!     \e[0m";
+    sleep 2
+}
+function copy(){
+    
+
+    
+    
+    rm -rf cws
+    mkdir cws
+    cd spm
+
+
+}
+read -p' OPTION එකක් තෝරන්න :- ' userinput1;
+if [ "${userinput1:-}" = "1" ]
+then
+cd && cd Evil-Spammer && cd spm && cat spm1
+echo
+echo
+while true; do
+
+    read -p "ආපහු Evil-Spammer ටුල් එකට යන්න ඕනෙ ද?    ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Evil-Spammer && bash spammer.sh  ; break;;
+
+        [Nn]* ) exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+fi
+if [ "${userinput1:-}" = "2" ]
+then
+   cd && cd Evil-Spammer && cd spm && cat spm2 
+   echo
+   echo
+   while true; do
+
+    read -p "ආපහු Evil-Spammer ටුල් එකට යන්න ඕනෙ ද?    ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Evil-Spammer && bash spammer.sh  ; break;;
+
+        [Nn]* ) exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+fi
+if [ "${userinput1:-}" = "3" ]
+then
+  cd && cd Evil-Spammer && cd spm && cat spm3  
+echo
+echo
+  while true; do
+
+    read -p "ආපහු Evil-Spammer ටුල් එකට යන්න ඕනෙ ද?    ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Evil-Spammer && bash spammer.sh  ; break;;
+
+        [Nn]* ) exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+fi
+if [ "${userinput1:-}" = "4" ]
+then
+    cd && cd Evil-Spammer && cd spm && cat spm4
+echo
+echo
+while true; do
+
+    read -p "ආපහු Evil-Spammer ටුල් එකට යන්න ඕනෙ ද?    ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Evil-Spammer && bash spammer.sh  ; break;;
+
+        [Nn]* ) exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+   
+fi
+if [ "${userinput1:-}" = "5" ]
+then 
+echo -e """\e[1;32m 
+
+             +-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+             |C|y|b|e|r| |W|a|r|r|i|o|r|s|
+             +-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+_______________________________________________________
+|                                                     |
+|                                                     |
+| [--] CREATED BY: JOHN KENER(OWNER OF CYBER WARRIORS)|
+|
+|                                                     |
+|
+| [--] Don't copyright this tool ..                   |
+|
+|                                                     |
+|
+:::::::::::::::::::::JOIN WITH US ::::::::::::::::::::|
+|                                                     |
+| [--] Telegram : https://t.me/By_sstp                |
+|                                                     |
+|                                                     |
+| [--] Whatsapp group 2 :                             |
+|  https://chat.whatsapp.com/DWMOhdZv78RHfYpmVfjBuS   |
+|                                                     |
+|                                                     |
+| [--] Youtube channel:                               |
+|http://m.youtube.com/channel/UCOC4YlK-7mb5jIbCRcuijvQ|
+|                                                     |
+|_____________________________________________________|
+
+"""
+echo
+while true; do
+
+    read -p "ආපහු Evil-Spammer ටුල් එකට යන්න ඕනෙ ද?    ( y /n ) :- " yn
+
+    case $yn in
+
+        [Yy]* ) cd && cd Evil-Spammer && bash spammer.sh  ; break;;
+
+        [Nn]* ) exit ;;
+
+        * ) echo "Please answer   yes    or    no.";;
+
+    esac
+
+done
+
+
+fi
+if [ "${userinput1:-}" = "6" ]
+then
+figlet THANK YOU  ...! |lolcat
+    cd
+fi
+
